3851cd12849276eea2426670495cfeaddf313b26
Dustami <lastivkaostap@gmail.com>
2026-05-02 22:48:51 +0300

    HISTORY OF ACTIONS

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
 # Python-Projects
 
 Repository of my small python projects that doesn\`t require their own repository. Here list of them:
+
 ## 1. Terminal-Calculator
+
 ### Whats new
-- Logarithms
-- Program command ‘round’ (used to configure rounding)
+
+- HISTORY!!!
+
+- Auto-saving actions into file
+
+- You can delete or read them
+
 ### Features
-- Basic commands (+, -, \* *, /)
+
+- History of actions
+
+- Basic commands (+, -, \* \*, /)
+
 - Advanced commands (%, ^, √)
+
 - Converter (length and temperature)
+
 - Trigonometry (sin, cos, tan, asin, acos, atan)
+
 - Logarithms (log, ln, lg)
+
 ### Todo
-- More operations
-- More settings
-- Make history of actions&settings
+
+1. Update list info (im too tired to make this today)
+
+2. Auto-delete of actions (I will try this but I think that wouldn\`t work)
+
+3. More operations (or not)
+
+4. More settings (if I get ideas)
+
+5. Make history of settings (json file so its will be on the end)
+
+6. Language choice (That will be hell)
--- Terminal_Calculator/Terminal_Calculator.py
+++ Terminal_Calculator/Terminal_Calculator.py
@@ -1,6 +1,7 @@
 #Libraries
 import re
 import math
+import json
 #Dictionaries
 basic_list = {
     '+': lambda a, b: a + b,
@@ -39,6 +40,7 @@ rounder = 12
 print('Welcome to Terminal Calculator!\nDon`t know commands? Try to print \'help\'!')
 while True:
     try:
+        result = None
         enter = input('> ').lower()
 #Operations
     #Program Commands
@@ -55,6 +57,12 @@ while True:
             print('Here list of all operations (\'x\' means your number)\n___Basic operations___\nx+x (for add)\nx-x (for subtract)\nx*x (for multiply)\nx/x (for divide)\n___Advenced operations___\nx^x (for power)\nsqrtx (for square root)\nx% of x (for finding the % of you number)\n___Convert operations___\nx cm to in (for converting centimeters to inches)\nx in to cm (for converting inches to centimeters)\nx m to ft (for converting meters to feets)\nx ft to m (for converting feet to meters)\nx km to mi (for converting kilometers to miles)\nx mi to km (for converting miles to kilometers)\nx C to F (for converting Celsius to Fahrenheit)\nx F to C (for converting Fahrenheit to Celsius)\n___Trigonometry operations___\nsin/cos/tan x deg/rad (for finding sinus or cosinus or tangens with degreees or radians)')
         elif enter == 'stop' or enter == 'exit':
             break
+        elif enter == 'history':
+            with open('History.txt', 'r', encoding='utf-8') as file:
+                print(file.read().strip())
+        elif enter == 'history delete' or enter == 'history clear':
+            with open('History.txt', 'w', encoding='utf-8') as file:
+                file.write('___HISTORY___\n')
     #Search
         basic_match = re.match(r'^\s*(\d+\.?\d*)\s*([-+*/^])\s*(\d+\.?\d*)\s*$', enter)
         sqrt_match = re.match(r'^\s*(sqrt)\s*(\d+\.?\d*)\s*$', enter)
@@ -67,24 +75,24 @@ while True:
             num1 = float(basic_match.group(1))
             operator = basic_match.group(2)
             num2 = float(basic_match.group(3))
-            print(f'= {round(basic_list[operator](num1, num2), rounder)}')
+            result = round(basic_list[operator](num1, num2), rounder)
         elif sqrt_match:
             num = float(sqrt_match.group(2))
-            print(f'= {round(math.sqrt(num1), rounder)}')
+            result = round(math.sqrt(num1), rounder)
         elif percent_match:
             percent = float(percent_match.group(1))
             total = float(percent_match.group(4))
-            print(f'= {round(total * percent / 100, rounder)}')
+            result = round(total * percent / 100, rounder)
         elif convert_match:
             num = float(convert_match.group(1))
             value1 = convert_match.group(2)
             value2 = convert_match.group(4)
-            print(f'= {round(convert_list[value1, value2](num), rounder)}')
+            result = round(convert_list[value1, value2](num), rounder)
         elif trigon_match:
             operator = trigon_match.group(1)
             num = float(trigon_match.group(2))
             dORr = trigon_match.group(3)
-            print(f'= {round(trigon_list[operator, dORr](num), rounder)}')
+            result = round(trigon_list[operator, dORr](num), rounder)
         elif log_match:
         #Has been error if you try to log_10 (try to use basic log with base 10)
         #So i decided to make this + its much shorter:
@@ -93,12 +101,18 @@ while True:
             if log == 'log':
                 try:
                     base = float(log_match.group(2))
-                    print(f'= {round(math.log(argument, base), rounder)}')
+                    result = round(math.log(argument, base), rounder)
                 except ValueError:
-                    print(f'= {round(math.log10(argument), rounder)}')
+                    result = round(math.log10(argument), rounder)
             elif log == 'ln':
-                print(f'= {round(math.log(argument, math.e), rounder)}')
+                result = round(math.log(argument, math.e), rounder)
             elif log == 'lg':
-                print(f'= {round(math.log2(argument), rounder)}')
+                result = round(math.log2(argument), rounder)
+    #Result print and auto write of history
+        if result != None:
+            result = str(f'= {result}')
+            print(result)
+            with open('History.txt', 'a', encoding='utf-8') as file:
+                file.write(f'|> {enter}\n|{result}\n')
     except ValueError:
         print('Value Error!')
